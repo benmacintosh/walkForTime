46a8c45d0a5312e8b10ac0b179b5b6e017442bdf
benmacintosh <ben.macintosh0@gmailcom>
2018-08-13 21:12:54 +1000

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+deployeable boilerplate of html (and progressive web app) of text and links
+links stay blue after clicked
+
+clone into own repo
+
+and title in index.html
+and icon.png 192x192 in public
+
+edit app.js
+
+npm build
+
+chagne  to your repo and push too github
---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+
+
+
+
+        <div>new</div>
+        <a href="https://google.com" style={{color:'blue'}}>google </a>
+        <p/>
+
+        <div>cv</div>
+        <p/>
+
+        <a href="https://www.instagram.com/0008000000macintosh/" style={{color:'blue'}}>https://www.instagram.com/0008000000macintosh/</a>
+
+
+
+
+      </div>
+    );
+  }
+}
+
+export default App;
