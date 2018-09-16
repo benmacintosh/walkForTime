3a1f4d2c130a9b22c108753c2c08b041bf99a762
benmacintosh <ben.macintosh0@gmailcom>
2018-09-16 14:26:32 +1000

    update

--- src/App.js
+++ src/App.js
@@ -57,15 +57,15 @@ class Algorithm extends PureComponent {
         this.state = {
             latlngs: [[-33.896919, 151.171899][-33.896425, 151.183834]],
             methods: ["public transport","walk"],
-            finalpoint: [],
+            finalpoint: {},
             que: [[]]
         }
 
-        this.componentDidMount = this.componentDidMount.bind(this)
+        this.componentDidMount = this.componentDidMount.bind(this);
+        this.setState = this.setState.bind(this);
     }
 
     componentDidMount(props){
-
         // dense grid of points, into priorirty que
         var left,right,top,bot;
         var density = 3;
@@ -74,7 +74,7 @@ class Algorithm extends PureComponent {
         console.log(lngdisc)
         var i, j;
         var initlat = -33.896919, initlng = 151.171899;
-        var thislat = -33.896919, thislng = 151.171899;
+        var thislat = initlat, thislng = initlng;
         var a = {lat: initlat, lng:initlng};
         var b = {lat:-33.896425, lng:151.171899}
 
@@ -86,14 +86,13 @@ class Algorithm extends PureComponent {
 
         var destinations = [];
         for(i=0; i<density;i++){
+            thislat = thislat+latdisc;
             for(j=0;j<density;j++){
+                thislng = thislng+lngdisc;
                 que.push([thislat,thislng]);
 
                 destinations.push({lat: thislat, lng: thislng});
-
-                thislng = thislng+lngdisc;
             }
-            thislat = thislat+latdisc;
             thislng = initlng;
         }
         console.log("que")
@@ -114,7 +113,7 @@ class Algorithm extends PureComponent {
         distanceapi.getDistanceMatrix({
             origins: as,
             destinations: destinations,
-            travelMode: 'DRIVING',
+            travelMode: 'TRANSIT',
             unitSystem: window.google.maps.UnitSystem.METRIC
         },function(response,status){
             if(status!=='OK'){
@@ -122,15 +121,69 @@ class Algorithm extends PureComponent {
             }else{
                 console.log("response")
                 console.log(response);
-                console.log(response.rows[0].elements[0].duration.value)
                 for(var i = 0; i<response.rows.length;i++){
-                    
+                    timesa.push(response.rows[i].elements[i].duration.value);
                 }
+                console.log(timesa)
+
+                // ignore if undefined
+            }
+        }
+        );
+
+        var that = this;
+        setTimeout(function(){
+        var distanceapi = new window.google.maps.DistanceMatrixService;
+        distanceapi.getDistanceMatrix({
+            origins: bs,
+            destinations: destinations,
+            travelMode: 'WALKING',
+            unitSystem: window.google.maps.UnitSystem.METRIC
+        },function(response,status){
+            if(status!=='OK'){
+                alert('Error was: '+ status);
+            }else{
+                console.log("response")
+                console.log(response);
+                for(var i = 0; i<response.rows.length;i++){
+                    timesb.push(response.rows[i].elements[i].duration.value);
+                }
+                console.log(timesb)
 
                 // ignore if undefined
             }
         }
         )
+
+
+
+        setTimeout(function(){
+        var mininddex = 0;
+        var minseconds = timesa[6]+timesb[6];
+        var thistime = 0;
+        for(var i = 0; i<timesa.length; i++){
+            thistime = timesa[i]+timesb[i];
+            if(thistime<minseconds){
+                console.log(mininddex)
+                minseconds=thistime;
+                mininddex=i;
+            }
+
+        }
+        console.log(mininddex)
+        console.log(destinations[mininddex])
+        that.setState({finalpoint: destinations[mininddex]});
+
+        },4000)
+
+    },11000);
+
+
+    }
+
+
+
+
 
 
         //TEMP JUST BRUTE SERACH
@@ -145,8 +198,6 @@ class Algorithm extends PureComponent {
         // sort by distance
 
 
-    }
-
     // iteratte through prioriety que search
 
 
@@ -158,7 +209,11 @@ class Algorithm extends PureComponent {
     {
         return(
             <div>
-            {this.state.finalpoint}
+            <div>newprfinlpt</div>
+            <div>
+            {this.state.finalpoint.lat}
+            {this.state.finalpoint.lng}
+            </div>
             </div>
             );
     }
@@ -209,21 +264,7 @@ export default class App extends PureComponent {
         <div>new</div>
         <a href="https://google.com" style={{color:'blue'}}>google</a>
         <div></div>
-        <a href="https://www.youtube.com/watch?v=MxwvtaswDYY" style={{color:'blue'}}>https://www.youtube.com/watch?v=MxwvtaswDYY</a> <div></div>
-        <a href="https://drive.google.com/open?id=1TkU_3pCP1_GgNQi1a_JKRGwaxE-HG3uZ" style={{color:'blue'}}>https://drive.google.com/open?id=1TkU_3pCP1_GgNQi1a_JKRGwaxE-HG3uZ </a>
-        <p/>
 
-        {/*<div>tools</div>
-
-        // <div>writing</div
-        */}
-
-        <div>cv</div>        
-        <a href="https://drive.google.com/open?id=1JobQ1qgPOFjpesU8tSAxJcxyaRh-dm8r" syle={{color:'blue'}}>https://drive.google.com/open?id=1JobQ1qgPOFjpesU8tSAxJcxyaRh-dm8r </a>
-        <p/>
-
-        <a href="https://www.instagram.com/0008000000macintosh/" style={{color:'blue'}}>https://www.instagram.com/0008000000macintosh/</a>
-        <div></div>
         <img src={img2} alt="" width="88"/>
         <img src={img} alt="" width="88"/>
 
